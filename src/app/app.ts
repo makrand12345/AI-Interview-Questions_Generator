1f24c086387d44da5b7d115f8360b9556afebe59
Makrand Vijay Girkhar <makrandgirkar43@gmail.com>
2026-01-02 20:31:25 +0530

    Deployment config

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // Required for router-outlet
 import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, Navbar], // Removed unused QuestionGenerator import
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
